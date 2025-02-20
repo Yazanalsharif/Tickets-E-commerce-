ea9a47889d2cb35eaa5967d4565b10dc3a0b10bd
Yazan alsharif <yazanalsharif@192.168.1.104>
2025-02-20 04:21:22 +0300

    Update

--- common/src/Events/Listener-kafka.ts
+++ common/src/Events/Listener-kafka.ts
@@ -38,9 +38,7 @@ export abstract class Listener1<T extends Event> {
         // Only if received data
         if (payload.message.value) {
           const data = this.parseData(payload.message.value);
-          if (data.ticket) {
-            data.ticket = this.parseData(data.ticket);
-          }
+
           await this.onMessage(data, payload);
         }
       },
